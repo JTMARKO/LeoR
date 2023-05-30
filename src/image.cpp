4a0a3c706add7a8ec43869361e36b5ccf11f946f
JT Markowitz <jtma@mit.edu>
2023-05-30 00:00:42 -0400

    Formatting

--- src/image.cpp
+++ src/image.cpp
@@ -1,19 +1,20 @@
 #include <iostream>
 #include "../include/image.hpp"
 
-Pixel get_pixel(Image image, int x, int y) {
-    const int index = image.width*y + x;
+Pixel get_pixel(Image image, int x, int y)
+{
+    const int index = image.width * y + x;
 
     return image.pixels[index];
-};
-
-void set_pixel(Image *image, int x, int y, Pixel pixel){
-    const int index = image->width*y + x;
-
-    image->pixels[index] = pixel;
-};
-
-void image_to_ppm(Image image) {
-
 }
 
+void set_pixel(Image *image, int x, int y, Pixel pixel)
+{
+    const int index = image->width * y + x;
+
+    image->pixels[index] = pixel;
+}
+
+void image_to_ppm(Image image)
+{
+}
