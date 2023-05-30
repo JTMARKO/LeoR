d707a23d939e7ee14698fe1589926eea9444eee8
JT Markowitz <jtma@mit.edu>
2023-05-30 00:00:21 -0400

    Added implmentation for get and set_pixel

--- src/image.cpp
+++ src/image.cpp
@@ -1,6 +1,19 @@
 #include <iostream>
 #include "../include/image.hpp"
 
-void draw_image(Image image) {
+Pixel get_pixel(Image image, int x, int y) {
+    const int index = image.width*y + x;
+
+    return image.pixels[index];
+};
+
+void set_pixel(Image *image, int x, int y, Pixel pixel){
+    const int index = image->width*y + x;
+
+    image->pixels[index] = pixel;
+};
+
+void image_to_ppm(Image image) {
 
 }
+
