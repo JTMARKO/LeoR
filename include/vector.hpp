2961f2740222a19af515f5d6652470f229890c3c
JT Markowitz <jtma@mit.edu>
2023-05-30 20:30:25 -0400

    Added rotate and normalize functions

--- include/vector.hpp
+++ include/vector.hpp
@@ -1,7 +1,13 @@
-//Simple struct representing a 3-dim vector
+// Simple struct representing a 3-dim vector
 struct Vector3
 {
     float x, y, z;
+};
+
+struct Ray3
+{
+    Vector3 origin;
+    Vector3 direction;
 };
 
 /**
@@ -19,3 +25,19 @@ float dot(Vector3 v1, Vector3 v2);
  * @returns float representing the magnitude of the vector
  */
 float magnitude(Vector3 v);
+
+/**
+ * Calculates a new, rotated vector around an axis
+ *
+ * @param angle angle in radians which vector is rotated
+ * @param vector pointer to a vector to be rotated
+ * @param axis pointer to a vector to be the axis of rotation
+ */
+Vector3 rotate(float angle, Vector3 *vector, Vector3 *axis);
+
+/**
+ * Normalizes a vector
+ *
+ * @param vector Vector3 object to be normalized
+ */
+void normalize(Vector3 *vector);
