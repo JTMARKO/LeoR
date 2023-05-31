5e2699a8deb4af97356f390d0c7d6b7ecfc70f19
JT Markowitz <jtma@mit.edu>
2023-05-30 20:36:27 -0400

    Changed to pass by reference and added implementation for normalize

--- src/vector.cpp
+++ src/vector.cpp
@@ -1,17 +1,26 @@
 #include <cmath>
 #include "../include/vector.hpp"
 
-float magnitude(Vector3 v)
+float magnitude(Vector3 *v)
 {
-    const float squared_sum = v.x * v.x + v.y * v.y + v.z * v.z;
+    const float squared_sum = v->x * v->x + v->y * v->y + v->z * v->z;
 
     return sqrt(squared_sum);
 }
 
-float dot(Vector3 v1, Vector3 v2)
+float dot(Vector3 *v1, Vector3 *v2)
 {
-    const float product = v1.x * v2.x + v1.y * v2.y + v1.z * v2.z;
+    const float product = v1->x * v2->x + v1->y * v2->y + v1->z * v2->z;
     const float normalizer = magnitude(v1) * magnitude(v2);
 
     return product / normalizer;
+}
+
+void normalize(Vector3 *v)
+{
+    const float mag = magnitude(v);
+
+    v->x /= mag;
+    v->y /= mag;
+    v->z /= mag;
 }
